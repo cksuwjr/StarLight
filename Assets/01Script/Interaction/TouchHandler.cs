8819463db80217411b11a86ae6a200a566c0ae11
차경훈 <cksuwjr@naver.com>
2025-05-22 13:06:43 +0900

    mobile

--- Assets/01Script/Interaction/TouchHandler.cs
+++ Assets/01Script/Interaction/TouchHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -28,8 +29,90 @@ public class TouchHandler : MonoBehaviour
         if (UIManager.Instance)
             if (UIManager.Instance.touchBlocking) return;
 
-//#if (UNITY_EDITOR || UNITY_EDITOR_WIN)
+#if (UNITY_EDITOR || UNITY_STANDALONE)
+        WinTouch();
+#endif
+        MobileTouch();
+    }
 
+    private void MobileTouch()
+    {
+        RaycastHit[] hit = null;
+
+        // 들어온 터치입력 수만큼 반복
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            // 들어온 터치 입력 순서대로 순회
+            Touch t = Input.GetTouch(i);
+
+            // UI를 터치하고 드래그 했을 때에는 회전하지 않도록 하기 위한 bool값
+            bool isTouchingUI = EventSystem.current.IsPointerOverGameObject(t.fingerId);
+
+            // 터치 입력의 상태에 따른 동작
+            switch (t.phase)
+            {
+                // 터치가 시작되었을 때
+                case UnityEngine.TouchPhase.Began:
+
+                    var ray = Camera.main.ScreenPointToRay(new Vector3(t.position.x, t.position.y, 0));
+
+                    hit = Physics.RaycastAll(ray, Mathf.Infinity, mask, QueryTriggerInteraction.UseGlobal);
+
+                    for (int j = 0; j < hit.Length; j++)
+                    {
+                        if (hit[j].collider == null) continue;
+                        if (hit[j].collider.isTrigger) continue;
+
+                        if (hit[j].collider.TryGetComponent<IInteract>(out var interact))
+                        {
+                            interact.Interaction();
+                            return;
+                        }
+                    }
+
+
+
+
+                    // 터치 입력이 화면의 오른쪽에서 이루어졌고
+                    // 아직 오른쪽 입력이 이루어지지 않았고
+                    // UI를 터치중이 아니라면
+                    if (t.position.x > halfScreenWidth && rightFingerId == -1 && !isTouchingUI)
+                    {
+                        // 오른쪽 터치 입력을 현재 터치입력의 번호로 저장
+                        rightFingerId = t.fingerId;
+                        // 터치 위치 저장
+                        lastTouchPosition = t.position;
+                    }
+                    break;
+                // 드래그 중일 때
+                case UnityEngine.TouchPhase.Moved:
+                    if (t.fingerId == rightFingerId && !isTouchingUI)
+                    {
+                        // 현재 위치와 이전 위치와의 거리 저장
+                        //Vector2 delta = t.position - lastTouchPosition;
+                        // cinemachine의 회전값에 거리 + 드래그 속도 만큼 더해줌
+                        if (cam.cameraMoving)
+                            cam.cameraMoving.Moving();
+                        // 최근 위치 갱신
+                        lastTouchPosition = t.position;
+                    }
+                    break;
+
+                // 손가락을 떼었을 때
+                case UnityEngine.TouchPhase.Ended:
+                    if (t.fingerId == rightFingerId)
+                    {
+                        // 오른쪽 터치 입력 번호 초기화
+                        rightFingerId = -1;
+                        //Debug.Log("오른쪽 손가락 끝");
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void WinTouch()
+    {
         RaycastHit[] hit = null;
         if (Input.GetMouseButtonDown(0))
         {
@@ -69,85 +152,5 @@ public class TouchHandler : MonoBehaviour
 
         if (Input.GetMouseButtonUp(0))
             onDrag = false;
-
-
-
-
-        //#elif (UNITY_IOS || UNITY_ANDROID)
-        //RaycastHit[] hit = null;
-
-        //// 들어온 터치입력 수만큼 반복
-        //for (int i = 0; i < Input.touchCount; i++)
-        //{
-        //    // 들어온 터치 입력 순서대로 순회
-        //    Touch t = Input.GetTouch(i);
-
-        //    // UI를 터치하고 드래그 했을 때에는 회전하지 않도록 하기 위한 bool값
-        //    bool isTouchingUI = EventSystem.current.IsPointerOverGameObject(t.fingerId);
-
-        //    RaycastHit[] hit2 = null;
-        //    // 터치 입력의 상태에 따른 동작
-        //    switch (t.phase)
-        //    {
-        //        // 터치가 시작되었을 때
-        //        case UnityEngine.TouchPhase.Began:
-
-        //            var ray = Camera.main.ScreenPointToRay(new Vector3(t.position.x, t.position.y, -Input.mousePosition.z));
-
-        //            hit2 = Physics.RaycastAll(ray, Mathf.Infinity, mask, QueryTriggerInteraction.UseGlobal);
-
-        //            for (int j = 0; j < hit.Length; j++)
-        //            {
-        //                if (hit2[j].collider == null) continue;
-        //                if (hit2[j].collider.isTrigger) continue;
-
-        //                if (hit2[j].collider.TryGetComponent<IInteract>(out var interact))
-        //                {
-        //                    interact.Interaction();
-        //                    return;
-        //                }
-        //            }
-
-
-
-
-        //            // 터치 입력이 화면의 오른쪽에서 이루어졌고
-        //            // 아직 오른쪽 입력이 이루어지지 않았고
-        //            // UI를 터치중이 아니라면
-        //            if (t.position.x > halfScreenWidth && rightFingerId == -1 && !isTouchingUI)
-        //            {
-        //                // 오른쪽 터치 입력을 현재 터치입력의 번호로 저장
-        //                rightFingerId = t.fingerId;
-        //                // 터치 위치 저장
-        //                lastTouchPosition = t.position;
-        //            }
-        //            break;
-        //        // 드래그 중일 때
-        //        case UnityEngine.TouchPhase.Moved:
-        //            if (t.fingerId == rightFingerId && !isTouchingUI)
-        //            {
-        //                // 현재 위치와 이전 위치와의 거리 저장
-        //                //Vector2 delta = t.position - lastTouchPosition;
-        //                // cinemachine의 회전값에 거리 + 드래그 속도 만큼 더해줌
-        //                if (cam.cameraMoving)
-        //                    cam.cameraMoving.Moving();
-        //                // 최근 위치 갱신
-        //                lastTouchPosition = t.position;
-        //            }
-        //            break;
-
-        //        // 손가락을 떼었을 때
-        //        case UnityEngine.TouchPhase.Ended:
-        //            if (t.fingerId == rightFingerId)
-        //            {
-        //                // 오른쪽 터치 입력 번호 초기화
-        //                rightFingerId = -1;
-        //                //Debug.Log("오른쪽 손가락 끝");
-        //            }
-        //            break;
-        //    }
-        //}
-
-        //#endif
     }
 }
